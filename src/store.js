a450a2c5d4cce858e918c812cdf3230c3ff86da5
Sanghyun Lee <shlee1027@daum.net>
2023-01-23 18:54:49 +0900

    first commit

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// let user = createSlice({
+//     name : 'user',
+//     initialState : 'kim'
+//   })
+
+export default configureStore({
+  reducer: {},
+});
